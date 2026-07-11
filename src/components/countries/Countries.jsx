3c2964cadcc4f8c0982e8422ff4304e224306b5c
MahfuzulIslamPranto <pranto.uiu.bd@gmail.com>
2026-07-11 12:02:57 +0600

    first commit

---
+++ src/components/countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>Countries of the world</h1>
+        </div>
+    );
+};
+
+export default Countries;
